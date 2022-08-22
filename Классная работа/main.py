114e0e17e60df5398a50346c39039e0183d3bd46
ecoo169 <yurausenko93@gmail.com>
2022-08-22 10:11:49 +0700

    .

---
+++ Классная работа/main.py
@@ -0,0 +1,19 @@
+#задача про МКАД
+a = int(input())
+b = int(input())
+print((a * b) % 109)
+
+
+x=float(input())
+
+s=float(abs(int(x)-x))
+
+print(s)
+
+
+
+x=float(input())
+
+s=int(float(abs(int(x)-x))*10)
+
+print(s)
